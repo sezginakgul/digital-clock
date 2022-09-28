2b813a990e28ef0ef16e3d784629181b2c79e25c
sezgin <sezgin.akgull@gmail.com>
2022-09-28 19:44:53 +0300

    added 'am' and 'pm'

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const timer = () => {
   let hours = new Date().getHours();
   let minute = new Date().getMinutes();
   let second = new Date().getSeconds();
+  let session = "AM";
+
+  if (hours == 0) {
+    hours = 12;
+  }
+
+  if (hours > 12) {
+    hours = hours - 12;
+    session = "PM";
+  }
+
   screen.innerText = `${hours < 10 ? "0" + hours : hours}:${
     minute < 10 ? "0" + minute : minute
-  }:${second < 10 ? "0" + second : second} ${hours >= 11 ? "PM" : "AM"}`;
+  }:${second < 10 ? "0" + second : second} ${session}`;
 };
 
 setInterval(timer, 1000);
